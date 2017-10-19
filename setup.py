58c9a40e21e80335be1b2e5df4110e6dd529ad33
Jon Purdy <jon@knowroaming.com>
2017-10-19 09:48:57 -0400

    Updated version number.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'author': 'Jon Purdy',
     'url': 'https://github.com/jonpurdy/nbssh/',
     'author_email': 'jon+nbssh@jonpurdy.com',
-    'version': '0.5dev1',
+    'version': '0.5',
     'install_requires': [
     'requests',
     'docopt',
